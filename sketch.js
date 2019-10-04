fcc829bdc09299c711c544e5efdab253f549a209
Juan Alonso <juan@neuronasmuertas.com>
2019-10-04 10:27:01 +0200

    JSON generation. Analyze small video segments instead of the whole video.

--- sketch.js
+++ sketch.js
@@ -10,21 +10,36 @@ let options = {
     multiplier: 0.75,
 }
 
-let playFrom = 205;
+let videoFile = 'Top Fifteen Female Ballet Dancers.mp4';
+let playFrom = 209;
+let playTo = 213.8;
+let frameCounter = 0;
+let minScore = 0.6;
+
 let isVideoPlaying = false;
+
+let json = {
+    frames:[]
+};
 
 function setup() {
 
     var canvas = createCanvas(1280, 720);
     canvas.parent('main');
 
-    video = createVideo(['video/Top Fifteen Female Ballet Dancers.mp4']);
+    video = createVideo(['video/' + videoFile]);
     video.onended(videoEnded);
     video.hide();
 
     buttonPlay = select('#buttonPlay');
     buttonPlay.hide();
     buttonPlay.mousePressed(togglePlay);
+
+    frameCounterElt = select('#frameCounter');
+    frameCounterElt.hide();
+
+    minScoreElt = select('#minScore');
+    minScoreElt.html('Min. score ' + minScore);
 
     poseNet = ml5.poseNet(video, options, modelReady);
     poseNet.on('pose', function(results) {
@@ -33,26 +48,6 @@ function setup() {
 
 }
 
-function togglePlay() {
-    if (!isVideoPlaying) {
-        video.play().time(playFrom);
-        buttonPlay.html('Stop');
-    } else {
-        video.stop().time(playFrom);
-        buttonPlay.html('Play');
-    }
-    isVideoPlaying = !isVideoPlaying;
-}
-
-
-function modelReady() {
-    buttonPlay.show();
-    video.time(playFrom);
-}
-
-
-function videoEnded() {}
-
 
 function draw() {
 
@@ -60,9 +55,53 @@ function draw() {
     if (poses.length > 0) {
         //drawSkeletons();
         drawJoints();
+        if (isVideoPlaying) {
+            json.frames.push({ "frame": frameCounter, "time": video.time(), "data": poses[0].pose.keypoints })
+            frameCounter++;
+        }
     }
 
+    if (isVideoPlaying && video.time() >= playTo) {
+
+        video.stop().time(playFrom);
+
+        json.filename = videoFile;
+        json.videoStart = playFrom;
+        json.videoEnd = playTo;
+        json.totalFames = frameCounter;
+        saveJSON(json, videoFile + '.json')
+
+        buttonPlay.html('Play');
+        frameCounter = 0;
+        isVideoPlaying = false;
+        console.log(json);
+    }
+    frameCounterElt.html('Frame ' + frameCounter);
 }
+
+
+function togglePlay() {
+
+    if (!isVideoPlaying) {
+        video.play().time(playFrom);
+        buttonPlay.html('Stop');
+    } else {
+        video.stop().time(playFrom);
+        buttonPlay.html('Play');
+        frameCounter = 0;
+    }
+    isVideoPlaying = !isVideoPlaying;
+}
+
+
+function modelReady() {
+    buttonPlay.show();
+    frameCounterElt.show();
+    video.time(playFrom);
+}
+
+
+function videoEnded() {}
 
 
 function drawSkeletons() {
@@ -78,15 +117,25 @@ function drawSkeletons() {
     }
 }
 
+
 function drawJoints() {
 
-
-    stroke(255);
-    strokeWeight(2);
+    noStroke();
 
     for (let i = 0; i < poses.length; i++) {
+
+        let radius;
+
+        if (poses[i].pose.score >= minScore) {
+            fill(255);
+            radius = 12;
+        } else {
+            fill(255, 120, 90);
+            radius = 7;
+        }
+
         for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
-            circle(poses[i].pose.keypoints[j].position.x, poses[i].pose.keypoints[j].position.y, 5);
+            circle(poses[i].pose.keypoints[j].position.x, poses[i].pose.keypoints[j].position.y, radius);
         }
     }
 }
